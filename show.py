c6d9724d37f82561954d63814797a8368ea429a8
李汶昇 <lisheng0706@gmail.com>
2017-12-28 14:47:46 +0800

    Add show namedWindow

--- show.py
+++ show.py
@@ -12,4 +12,12 @@ cv2.imshow('My Image', img)
 # 函數是用來等待與讀取使用者按下的按鍵，而其參數是等待時間（單位為毫秒），若設定為 0 就表示持續等待至使用者按下按鍵為止
 cv2.waitKey(0)
 # 關閉所有 OpenCV 的視窗
-cv2.destroyAllWindows('My Image')
+cv2.destroyAllWindows()
+# 關閉 'My Image' 視窗
+cv2.destroyWindow('My Image')
+
+# 讓視窗可以自由縮放大小
+cv2.namedWindow('My Image1', cv2.WINDOW_NORMAL)
+cv2.imshow('My Image1', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
