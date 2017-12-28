156769f44b7f3b2f731fc35ccf33d40654157081
李汶昇 <lisheng0706@gmail.com>
2017-12-28 14:33:03 +0800

    openCV show

--- show.py
+++ show.py
@@ -12,4 +12,4 @@ cv2.imshow('My Image', img)
 # 函數是用來等待與讀取使用者按下的按鍵，而其參數是等待時間（單位為毫秒），若設定為 0 就表示持續等待至使用者按下按鍵為止
 cv2.waitKey(0)
 # 關閉所有 OpenCV 的視窗
-cv2.destroyAllWindows()
+cv2.destroyAllWindows('My Image')
